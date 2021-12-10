eff918d885da0348a43b9882637ccf098ae3f0ec
daicoden <daicoden@copypastel.com>
2021-12-09 19:28:30 -1000

    star1

---
+++ day10/day10.py
@@ -0,0 +1,52 @@
+def get_data(filename):
+    with open(filename) as f:
+        data = list(map(lambda line: line.rstrip(), f.readlines()))
+    return data
+
+
+data = get_data("input.txt")
+
+open_values = ['(', '[', '{', '<']
+close_values = [')', ']', '}', '>']
+
+
+def validate(line, stack, index=0):
+    #  If we're incemplete
+    if len(line) == index and len(stack) != 0:
+        return False
+
+    if len(line) == index:
+        return None
+
+    if line[index] in open_values:
+        stack.append(line[index])
+        return validate(line, stack, index + 1)
+
+    token_index = close_values.index(line[index])
+    if open_values[token_index] == stack[-1]:
+        stack.pop()
+        return validate(line, stack, index + 1)
+
+    return line[index]
+
+
+errored_data = []
+
+SCORES = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+
+score=0
+for line in data:
+    result = validate(line, [], 0)
+    if result is not None:
+        if result != False:
+            score += SCORES[result]
+            errored_data.append(result)
+    else:
+        print("good line")
+
+print(score)
